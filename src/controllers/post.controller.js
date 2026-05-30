462df09af7bffc6ed02db12b8a2781052efcc66d
Tarun1204c <tarunpandav5@gmail.com>
2026-05-30 12:57:03 +0530

    add get post controller

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -4,7 +4,6 @@ const {toFile} = require('@imagekit/nodejs')
 const jwt = require('jsonwebtoken')
 
 
-
 const imagekit = new ImageKit({
     privateKey : process.env.IMAGEKIT_PRIVATE_KEY
 })
@@ -58,6 +57,79 @@ async function createPostController(req, res) {
 
 }
 
+async function getPostController(req,res) {
+
+    const token = req.cookies.token
+
+    let decoded = null
+
+    try{
+         decoded = jwt.verify(token, process.env.JWT_SECRET)
+
+    }catch(err){
+        return res.status(401).json({
+            message:"token invalid"
+        })
+
+    }
+
+    const userId = decoded.id
+
+    const posts = await postModel.find({
+        user : userId
+    })
+
+    res.status(200).json({
+        message : "psot fetched sucessfully" ,
+        posts  
+    })
+
+}
+
+async function getPostDetails(req, res){
+    const token = req.cookies.token
+
+    if(!token){
+        return res.status(401).json({
+            message : "Unauthorized acesses"
+        })
+    }
+    let decoded = null;
+
+    try{
+        decoded = jwt.verify(token,process.env.JWT_SECRET)
+
+    }catch(err){
+        return res.status(401).json({
+            message : "Invalid token"
+        })
+    }
+    const userId = decoded.id
+    const postId = req.params.postId
+
+    const post = await postModel.findById(postId)
+
+    if(post){
+        return res.status(404).json({
+            message: "Post not found."
+        })
+    }
+    const isValidUser = post.user.toString() === userId
+
+    if(!isValidUser){
+        return res.status(403).json({
+            message:"Forbidden Content"
+        })
+    }
+
+    return res.status(200).json({
+        message : "Post Fetched Sucessfully" ,
+        post
+    })
+}
+
 module.exports = {
-    createPostController
+    createPostController,
+    getPostController,
+    getPostDetails
 }
