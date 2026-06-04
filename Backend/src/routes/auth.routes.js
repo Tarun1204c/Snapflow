9511dd8155fa95d9c153028915919d6ff0631f66
Tarun1204c <tarunpandav5@gmail.com>
2026-06-04 23:20:39 +0530

    getmeController

--- Backend/src/routes/auth.routes.js
+++ Backend/src/routes/auth.routes.js
@@ -1,11 +1,15 @@
+
 const express = require('express')
 const authController = require("../controllers/auth.controllers");
 const { registerController, loginController } = require('../controllers/auth.controllers');
+const identifyUser = require("../middleware/auth.middleware")
 
 const authRouter = express.Router();
 
 authRouter.post('/register', registerController) 
     
 authRouter.post("/login", loginController)
+
+authRouter.get("/get-me",identifyUser, authController.getMeController)
 
 module.exports = authRouter
