4717d287ec8e255d5673104a8bfd336f815902b7
Tarun1204c <tarunpandav5@gmail.com>
2026-06-16 23:47:18 +0530

    new features

--- Frontend/src/features/auth/pages/register.jsx
+++ Frontend/src/features/auth/pages/register.jsx
@@ -1,37 +1,50 @@
 import React, {useState} from 'react'
 import '../styles/form.scss'
-import {Link} from 'react-router'
+import {Link, useNavigate} from 'react-router'
 import axios from 'axios'
 import { useAuth } from '../hooks/use.auth'
-import { register } from '../services/auth.api'
 
 const Register = () => {
 
+    const {loading, handleRegister} = useAuth()
+
     const [username,setUsername] = useState("")
-    const[email, setEmail] = useState("")
-    const [password, setPassword] = useState("")
+    const [email, setEmail] = useState("")
+    const[password, setPassowrd] = useState("")
 
+    const navigate = useNavigate()
 
-    async function handleSubmit(e){
+    const handleSubmit =  async (e) => {
         e.preventDefault()
+
+       await handleRegister(username, email, password)
+       navigate('/')
     }
-  return (
+
+
+    if(loading){
+        return (<main><h1>Loading...</h1></main>)
+    }
+    
+
+
+    return (
     <main>
         <div className="form-container">
             <h1>SnapFlow</h1>
             <form className='form' onSubmit={handleSubmit}>
                 <input
-                onInput={(e)=>{setUsername(e.target.value)}}
+                onChange={(e)=>{setUsername(e.target.value)}}
                  type="text" 
                  name="username" 
                  placeholder="Enter a username"  />
                 <input
-                onInput={(e)=>{setEmail(e.target.value)}}
+                onChange={(e)=>{setEmail(e.target.value)}}
                 type="text" 
                 name="email" 
                 placeholder="Enter a email" />
                 <input  
-                onInput={(e)=> {setPassword(e.target.value)}}
+                onChange={(e)=> {setPassword(e.target.value)}}
                 type="text" 
                 name="Paasword" 
                 placeholder="Enter a Paasword" />
