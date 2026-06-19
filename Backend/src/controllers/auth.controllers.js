f7e713bb8a1f7a97041f35222a6eea671a01e721
Tarun1204c <tarunpandav5@gmail.com>
2026-06-19 20:48:14 +0530

    select method

--- Backend/src/controllers/auth.controllers.js
+++ Backend/src/controllers/auth.controllers.js
@@ -66,7 +66,7 @@ async function loginController(req,res) {
                 email : email
             }
         ]    
-    })
+    }).select("+password")
 
     if(!user) {
         return res.status(404).json({
