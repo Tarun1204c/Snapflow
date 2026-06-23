4085f73acba1bc2bf5ba6f752a5811e9f6e7e3e8
Tarun1204c <tarunpandav5@gmail.com>
2026-06-23 23:25:23 +0530

    feed.scss

--- Frontend/src/features/post/components/Post.jsx
+++ Frontend/src/features/post/components/Post.jsx
@@ -1,12 +1,9 @@
 import React from 'react'
-import "../style/feed.scss"
 
-const Feed = () => {
+const Post = () => {
   return (
-    <main className='feed-page'>
-      <div className="feed">
-        <div className='posts'>
-          <div className="post">
+
+        <div className="post">
             <div className="user">
               <div className="img-wranpper">
                <img src='https://images.unsplash.com/photo-1780789594812-7828eae1ea07?w=1600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxMHx8fGVufDB8fHx8fA%3D%3D' alt='' />
@@ -28,12 +25,8 @@ const Feed = () => {
             <div className='bottom'>
               <p className='caption'>caption caption</p>
             </div>
-          </div>
         </div>
-      </div>
-    </main>
-  )  
+  )
 }
 
-export default Feed
-
+export default Post
