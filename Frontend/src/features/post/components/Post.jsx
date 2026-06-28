2caef03ff225c823c6d2a8b823419646d72eac0d
Tarun1204c <tarunpandav5@gmail.com>
2026-06-28 15:23:55 +0530

    user profile

--- Frontend/src/features/post/components/Post.jsx
+++ Frontend/src/features/post/components/Post.jsx
@@ -6,7 +6,7 @@ const Post = ({user, post}) => {
         <div className="post">
             <div className="user">
               <div className="img-wranpper">
-               <img src='https://images.unsplash.com/photo-1780789594812-7828eae1ea07?w=1600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxMHx8fGVufDB8fHx8fA%3D%3D' alt='' />
+               <img src={user.profile} alt='' />
               </div> 
               <p>Username</p>
             </div>
